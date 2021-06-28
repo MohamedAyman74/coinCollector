c88d22374090c0a09ca2d1e426ede6a3bc5c95c8
MohamedAyman74 <mohamedaymanmoudy1@gmail.com>
2021-06-28 22:01:48 +0200

    Added new abilities and fixed players going out of their screen width and height

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const coin = document.querySelector("#coin");
 const player = document.querySelector("#player");
 const avatar = document.querySelector("#avatar");
 const redCoin = document.querySelector("#redCoin");
+const images = document.querySelector("img");
 
 redCoin.style.display = "none";
 
@@ -18,9 +19,13 @@ function isTouching(a, b) {
 }
 player.style.top = "100px";
 player.style.left = "100px";
+images.style.width = "50px";
+player.style.width = "50px";
 let score = document.querySelector("#score");
+let shrink = document.querySelector("#shrink");
+shrink.innerText = 5;
 score.innerText = 0;
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keypress", function (e) {
 	let vertical = 0;
 	let horizontal = 0;
 	if (e.key === "ArrowDown" || e.key === "s") {
@@ -34,6 +39,15 @@ window.addEventListener("keydown", function (e) {
 	}
 	else if (e.key === "ArrowRight" || e.key === "d") {
 		horizontal += 50;
+	}
+	else if (e.key === "5") {
+		if(images.style.width == "50px"){
+			if (shrink.innerText > 0) {
+				images.style.width = "10px";
+				window.setInterval(() => images.style.width = "50px", 7000);
+				shrink.innerText--;
+			}
+		}
 	}
 	move(vertical, horizontal);
 	if (isTouching(coin, avatar)) {
@@ -63,8 +77,16 @@ const displayRedCoin = () => {
 const move = (el, am) => {
 	const ver = getPos(player.style.top);
 	const hor = getPos(player.style.left);
+	if (ver + el > window.innerHeight -100 || ver + el < -100) {
+		el = 0;
+	}
+	else if (hor + am > window.innerWidth -100 || hor + am < -100) {
+		am = 0;
+	}
+
 	player.style.top = `${ver + el}px`;
 	player.style.left = `${hor + am}px`;
+	console.log(window.innerHeight);
 	console.log(player.style.top);
 	if (am < 0)
 		avatar.style.transform = "scaleX(-1)";
@@ -77,8 +99,8 @@ const getPos = (item) => {
 }
 
 const moveCoin = (item) => {
-	const height = Math.floor(Math.random() * window.innerHeight);
-	const width = Math.floor(Math.random() * window.innerWidth);
+	const height = Math.abs(Math.floor(Math.random() * window.innerHeight - 300));
+	const width = Math.abs(Math.floor(Math.random() * window.innerWidth - 300));
 	// const coin = document.querySelector("#coin");
 	item.style.top = `${height}px`;
 	item.style.left = `${width}px`;
